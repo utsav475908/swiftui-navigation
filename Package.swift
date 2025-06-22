a088d023beadf60c9eac211f2ea9ee66902ee780
Kumar Utsav <aveckumar@gmail.com>
2025-06-22 16:55:03 +0530

    Update Package.swift
    Updated the commit version

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.4"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.2"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.4.3"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
   ],
   targets: [
